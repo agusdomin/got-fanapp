8551c765d8f9378d6fb1e1d0a752aafaeb9eaa20
agus <agussebastiandominguez@gmail.com>
2022-06-04 00:22:24 -0300

    comentarios sobre uso de Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Creacion de un servicio que se encarga de la consulta a la API
+
+// Crear de cubit, que extiende de cubir declarando el tipo de estado
+  //Cambio de estado con las subclases creadas que heredan y definen el estado general.
+
+// Cuando necesito que un widget conozca y maneje el estado, este tendra un BlocBuilder, que definira el 
+// cubit y el estado que maneja.
+  // usa un builder que tiene el contexto y el estado, asi puedo acceder a los distintos estados y realizar lo que quiera.
+  
+//El tipo de estado herada de equatable
+  // defino los parametros que van a detemerminar mi estado y a los que voy a escuchar para saber si hubo cambio de estado\
+  // creo distintos estados para diferenciar
